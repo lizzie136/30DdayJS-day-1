1eeeeeaea9f19a1498c8602984d025ebf87c0e64
lizzie136 <darknessover136@gmail.com>
2019-06-01 15:38:48 -0500

    Reformat app.js to meet configurations

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
+'use strict';
+
 function playSound(event) {
   console.log(event.keyCode);
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
   if (!audio) return;
-  console.log(audio);
-  console.log(key);
 
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
 }
 
-window.addEventListener('keydown', playSound);
-
 function removeTransition(event) {
   if (event.propertyName !== 'transform') return;
   this.classList.remove('playing');
 }
-const keys = document.querySelectorAll('.key')
+
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll('.key');
 keys.forEach(key => {
   key.addEventListener('transitionend', removeTransition);
 });
-// window.addEventListener('');
